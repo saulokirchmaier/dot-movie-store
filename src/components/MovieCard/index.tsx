8fd0b32594fd9906245e053fbd57cfdcb005acba
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2025-10-07 14:19:10 -0300

    feat: movie card

---
+++ src/components/MovieCard/index.tsx
@@ -0,0 +1,67 @@
+import { Heart, Star } from 'lucide-react';
+import { Button } from '../ui/button';
+import Magnet from '../ui/Magnet';
+
+interface MovieCardProps {
+  id: string;
+  imageURL: string;
+  title: string;
+  genre: string;
+  price: string;
+  date: string;
+  rating: number;
+}
+
+const MovieCard = ({
+  id,
+  imageURL,
+  title,
+  genre,
+  price,
+  date,
+  rating,
+}: MovieCardProps) => {
+  console.log(id);
+  return (
+    <Magnet padding={20} disabled={false} magnetStrength={50}>
+      <div className="w-[260px] bg-gray-50 rounded-lg shadow-md hover:shadow-lg hover:shadow-green-300 hover:inset-shadow-sm transform hover:scale-102 transition-transform duration-300">
+        <div className="w-full h-sm bg-gray-100 rounded-lg relative">
+          <img
+            src={imageURL}
+            alt="Movie Poster"
+            className="w-[300px] h-[340px] object-cover rounded-t-lg"
+          />
+          <Heart
+            className="absolute top-2 right-2 cursor-pointer hover:scale-110 transition-transform"
+            // fill="red"
+            stroke="red"
+            size={24}
+          />
+          <div className="absolute bottom-2 w-full flex justify-center">
+            <p className="text-md text-white font-bold bg-gray-950/50 rounded-lg px-4 py-2">
+              {date}
+            </p>
+          </div>
+        </div>
+        <div className="py-2 px-4 flex flex-col gap-1">
+          <h2 className="text-lg font-bold text-gray-900">{title}</h2>
+          <div className="flex items-center justify-evenly">
+            <div className="flex items-center gap-2">
+              <Star size={24} fill="#ffd700" stroke="#ffd700" />
+              <p className="text-md text-gray-700 font-bold">{rating}</p>
+            </div>
+            <p className="text-sm text-gray-500">{genre}</p>
+          </div>
+          <div>
+            <p className="text-md text-gray-500">R$ {price}</p>
+          </div>
+        </div>
+        <Button className="w-full bg-emerald-700 hover:bg-emerald-500 transition-colors duration-300 cursor-pointer">
+          <p className="text-md text-white font-bold">Adicionar</p>
+        </Button>
+      </div>
+    </Magnet>
+  );
+};
+
+export default MovieCard;
